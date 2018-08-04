f405c993f286ef5389b223295a09449b13783ece
Unknown <supakorn.trakulmaiphol@mail.kmutt.ac.th>
2018-08-04 12:58:09 +0700

    [ADD] Game Constructor และการสร้างกระดาน

--- XO-Project/test/model/GameTest.java
+++ XO-Project/test/model/GameTest.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
  * @author bank2
  */
 public class GameTest {
-    
+
     public GameTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -44,12 +44,17 @@ public class GameTest {
     public void testCheckDiagonal() {
         Game g = new Game();
     }
+
     @Test
-    public void testAddSymbol(int row, int column){
+    public void testAddSymbol(int row, int column) {
         Game g = new Game();
         g.createBoard();
         g.addSymbol(0, 0);
         //assertEquals('x', g.getSymBolFromBoard(0, 0));
     }
-    
+
+    public void testCheckHorizontal() {
+
+    }
+
 }
