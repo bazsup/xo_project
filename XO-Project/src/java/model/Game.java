0a28cbb3b4496afd55575d388c5a3f2449f8356f
Unknown <supakorn.trakulmaiphol@mail.kmutt.ac.th>
2018-08-04 12:49:53 +0700

    [ADD] Game Constructor สร้างผู้เล่นกับ Board

--- XO-Project/src/java/model/Game.java
+++ XO-Project/src/java/model/Game.java
@@ -7,16 +7,27 @@ package model;
 
 /**
  *
-<<<<<<< HEAD
  * @author bank2
  */
 public class Game {
 
+    Player player1;
+    Player player2;
+    
+    // char พื้นฐานเป็น จ 
     char board[][];
     char currentPlayer = 'x';
     
-    Player player1;
-    Player player2;
+
+    public Game() {
+        this.player1 = new Player();
+        this.player2 = new Player();
+        createBoard();
+    }
+
+    public void createBoard() {
+        board = new char[3][3];
+    }
 
     public boolean checkDiagonal() {
         boolean result = false;
@@ -26,10 +37,6 @@ public class Game {
             result = true;
         }
         return result;
-    }
-    
-    public void createBoard(){
-        board = new char[3][3];
     }
     
     public boolean addSymbol(int row, int column){
@@ -43,9 +50,21 @@ public class Game {
     public char getSymBolFromBoard(int row, int column){
         return board[row][column];
     }
-    
+
     public void checkHorizontal() {
-        
+        boolean result = false;
+        if ( board[0][0] == board[0][1] & board[0][1] ==board[0][2] ) {
+            result = true;
+        }else if( board[1][0] == board[1][1] & board[1][1] ==board[1][2]){
+            result = true;
+        }else if( board[2][0] == board[2][1] & board[2][1] ==board[2][2]){
+            result = true;
+        }
+    }
+
+    // Servelet
+    public static void main(String[] args) {
+
     }
 
 }
